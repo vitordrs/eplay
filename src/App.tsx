4b92ae45e6c65aa53616674c2b2a9110ba452817
vitordrs <soaresvitor196@gmail.com>
2026-01-07 13:54:24 -0300

    Criando tag e botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />
+  }
+])
 
 function App() {
   return (
@@ -9,7 +17,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,13 +1,25 @@
 import { Imagem, Titulo, Precos } from './styles'
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
-      <Precos>
-        De <s>R$ 250,00</s> <br /> por apenas R$ 99,90
-      </Precos>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Precos>
+          De <s>R$ 250,00</s> <br /> por apenas R$ 99,90
+        </Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique para aproveitar a orferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
